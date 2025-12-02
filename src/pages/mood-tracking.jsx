ab3cb917caae67d84c8ebbbd4f2e3a0ea0383e97
lorelaiah <lorelaih@uw.edu>
2025-12-02 13:32:28 -0800

    moods ordered

--- src/pages/mood-tracking.jsx
+++ src/pages/mood-tracking.jsx
@@ -1,22 +1,33 @@
 
 import Mood from "../components/mood.jsx";
 import { db } from "../firebase/config.js";
-import { useState } from "react";
-import { collection, doc } from "firebase/firestore";
-import { useAuth } from "../components/authContext.jsx";
+import { useState, useEffect } from "react";
+import { collection, doc, getDocs, query, setDoc, onSnapshot, orderBy } from "firebase/firestore";
+import { useAuth } from "../components/authContext.jsx"; 
 import { Link } from "react-router-dom";
 
 
 const MoodTracking = () => {
     const [moodIds, setMoodIds] = useState([]);
     const { currentUser, loading } = useAuth();
+    
+    useEffect(() => {
+        let unsubscribe = () => {};
+        if (currentUser) {
+            const collectionRef = collection(db, "users", currentUser.uid, "moods");
+            const order = query(collectionRef, orderBy("timestamp", "desc"));
+            unsubscribe = onSnapshot(order, (querySnapshot) => {
+                const updatedMoodIds = querySnapshot.docs.map(doc => doc.id);
+                setMoodIds(updatedMoodIds);
+            }, (error) => {
+                console.error("Error listening for moods:", error);
+                setMoodIds([]);
+            });
+        }
 
-    const newMood = () => {
-        const newDocRef = doc(collection(db, "days"));
-        const newId = newDocRef.id;
-        setMoodIds(prevIds => [...prevIds, newId]);
-        console.log("added");
-    }
+        return () => unsubscribe(); 
+        
+    }, [currentUser]);
 
     if (loading) {
         return (
@@ -33,13 +44,22 @@ const MoodTracking = () => {
         )
     };
 
+
+    const newMood = async () => {
+        const newDoc = doc(collection(db, "users", currentUser.uid, "moods"));
+        await setDoc(newDoc, {
+            timestamp : new Date()
+        });
+        console.log("added");
+    }
+
     return (
         <div>
             <h1>mood</h1>
             <button onClick={newMood}>log new mood</button>
             {moodIds.map((id) => (
-                <div>
-                <Mood moodId={id} user={currentUser.uid}/>
+                <div key={id}>
+                <Mood moodId={id} />
                 </div>
             ))}            
         </div>
