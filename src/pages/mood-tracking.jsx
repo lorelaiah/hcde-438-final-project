c55680a87ee4a08d683eebd56619ff5f4005ff8a
lorelaiah <lorelaih@uw.edu>
2025-12-02 11:21:40 -0800

    update sign in/sign out/sign up

--- src/pages/mood-tracking.jsx
+++ src/pages/mood-tracking.jsx
@@ -3,10 +3,13 @@ import Mood from "../components/mood.jsx";
 import { db } from "../firebase/config.js";
 import { useState } from "react";
 import { collection, doc } from "firebase/firestore";
+import { useAuth } from "../components/authContext.jsx";
+import { Link } from "react-router-dom";
 
 
 const MoodTracking = () => {
     const [moodIds, setMoodIds] = useState([]);
+    const { currentUser, loading } = useAuth();
 
     const newMood = () => {
         const newDocRef = doc(collection(db, "days"));
@@ -15,13 +18,28 @@ const MoodTracking = () => {
         console.log("added");
     }
 
+    if (loading) {
+        return (
+            <div>loading...</div>
+        );
+    }
+
+    if (!currentUser) {
+        return (
+            <div>
+                <p>login to log mood</p>
+                <Link to="/login"> login page </Link>
+            </div>
+        )
+    };
+
     return (
         <div>
             <h1>mood</h1>
             <button onClick={newMood}>log new mood</button>
             {moodIds.map((id) => (
                 <div>
-                <Mood moodId={id}/>
+                <Mood moodId={id} user={currentUser.uid}/>
                 </div>
             ))}            
         </div>
