265505d2dc0695359b45796cbc4602398a039180
limclynn <clim1@uw.edu>
2025-12-02 21:28:05 -0800

    PLEASE WORKKK API

--- src/pages/recommendations.jsx
+++ src/pages/recommendations.jsx
@@ -9,24 +9,20 @@ const Recommendations = () => {
     const [quote, setQuote] = useState(null);
 
     useEffect(() => {
-        const getQuote = async () => {
-            try {
-                const response = await fetch("https://api.api-ninjas.com/v1/quotes", {
-                    headers: { 
-                        "X-Api-Key": "Enter key here"
-                    }
-                });
-                const data = await response.json();
+    const getQuote = async () => {
+        try {
+            const response = await fetch("https://api.api-ninjas.com/v1/randomquotes", {
+                headers: { "X-Api-Key": "YOUR_API_KEY_HERE" }
+            });
+            const data = await response.json();
+            setQuote(data[0]);
+        } catch (err) {
+            console.log("Error fetching quote:", err.message);
+        }
+    };
+    getQuote();
+}, []);
 
-                const q = data[0];
-                setQuote(q);
-            } catch (err) {
-                console.log("Error fetching quote:", err.message);
-            }
-        };
-
-        getQuote();
-    }, []);
 
     if (loading) {
         return <div>that one error again unfort</div>;
